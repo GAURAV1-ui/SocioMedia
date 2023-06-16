7dd770823e43ed2b8a03ad909ee86f05f82ecf07
GAURAV <iamgaurav201@gmail.com>
2023-06-16 15:37:37 +0530

    new commit

--- routes/posts.js
+++ routes/posts.js
@@ -7,4 +7,6 @@ const router = express.Router();
 router.get("/", verifyToken, getFeedPosts);
 router.get("/:userId/posts", verifyToken, getUserPosts);
 
-router
+router.patch("/:id/like", verifyToken, likePost);
+
+export default router;
